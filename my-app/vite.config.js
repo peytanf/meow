7c93e277b126c03399187c90dd29f3b62e5a77f0
2236914 <22-36914@g.batstate-u.edu.ph>
2025-05-10 00:45:00 +0800

    Update vite.config.js with correct base path

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/meowww/', // 👈 necessary for GitHub Pages deployment
+  base: '/meow/', // 👈 necessary for GitHub Pages deployment
   plugins: [react()],
   server: {
     port: 3000,
